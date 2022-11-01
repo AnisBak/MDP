3686e54d61a30744d1048482bec5f7287ac4fa4c
Anis <anis.bakhtaoui@gmail.com>
2022-10-31 21:58:43 -0500

    Home page done Lesson 5 homework

--- lesson5_homework/app/src/main/java/com/anis/lesson5_homework/MainActivity.kt
+++ lesson5_homework/app/src/main/java/com/anis/lesson5_homework/MainActivity.kt
@@ -2,10 +2,40 @@ package com.anis.lesson5_homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+    lateinit var email:EditText
+    lateinit var password:EditText
+    lateinit var signIn:Button
+
+    private var listUsers =  ArrayList<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        signIn = findViewById(R.id.sign_in)
+
+        listUsers.addAll(listOf(
+            User("anis@mail.com","123"),
+            User("jimmy@mail.com","123"),
+            User("jack","123"),
+            User("Julia","123"),
+            User("Ken","123")))
+
+        signIn.setOnClickListener {
+            if (listUsers.contains(User(email.text.toString(),password.text.toString()))){
+                Toast.makeText(this, "Sign in successful", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Sign in NOT successful", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
     }
 }
---
+++ lesson5_homework/app/src/main/java/com/anis/lesson5_homework/User.kt
@@ -0,0 +1,4 @@
+package com.anis.lesson5_homework
+
+data class User(val username:String,
+                val password:String)
